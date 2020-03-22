2b2c2dd64f8949361c7f87fc159656bf0c4eb414
Akbar Binaji <akbar.binaji@gmail.com>
2020-03-22 19:18:37 +0800

    create Case model

--- models/case.rb
+++ models/case.rb
@@ -1,4 +1,15 @@
 require 'active_record'
 
 class Case < ActiveRecord::Base
+  class << self
+  
+    def to_object(data)
+      self.new(
+        infected: data[:jumlahKasus],
+        active: data[:perawatan],
+        recovered: data[:sembuh],
+        death: data[:meninggal]
+      )
+    end
+  end
 end
