0eab6d58b9e3088701c539c51d93b435ddbdcd07
Akbar Binaji <akbar.binaji@gmail.com>
2020-03-22 19:19:16 +0800

    update by replacing chat detail

--- lib/message_sender.rb
+++ lib/message_sender.rb
@@ -4,26 +4,28 @@ require './lib/app_configurator'
 class MessageSender
   attr_reader :bot
   attr_reader :text
-  attr_reader :chat
+  attr_reader :userid
+  attr_reader :username
   attr_reader :answers
   attr_reader :logger
 
   def initialize(options)
     @bot = options[:bot]
     @text = options[:text]
-    @chat = options[:chat]
+    @userid = options[:userid]
+    @username = options[:username]
     @answers = options[:answers]
     @logger = AppConfigurator.new.get_logger
   end
 
   def send
     if reply_markup
-      bot.api.send_message(chat_id: chat.id, text: text, reply_markup: reply_markup)
+      bot.api.send_message(chat_id: userid, text: text, reply_markup: reply_markup)
     else
-      bot.api.send_message(chat_id: chat.id, text: text)
+      bot.api.send_message(chat_id: userid, text: text)
     end
 
-    logger.debug "sending '#{text}' to #{chat.username}"
+    logger.debug "sending '#{text}' to #{username}"
   end
 
   private
