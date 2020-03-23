93810df590c7108d4f14d241b465a1184a48a283
Akbar Binaji <akbar.binaji@gmail.com>
2020-03-23 22:09:54 +0800

    update by changing update method for cases data

--- lib/notification_sender.rb
+++ lib/notification_sender.rb
@@ -49,7 +49,7 @@ class NotificationSender
       new_cases_data(data_source).save
     else
       unless compare_data?(exist_cases_data, new_cases_data(data_source))
-        Case.update(exist_cases_data.id, new_cases_data(data_source))
+        update_cases_data(exist_cases_data.id, new_cases_data(data_source))
 
         return true
       end
@@ -62,6 +62,15 @@ class NotificationSender
     Case.to_object(data_source)
   end
 
+  def update_cases_data(data_id, update_data)
+    Case.update(data_id, 
+      infected: update_data.infected,
+      active: update_data.active,
+      recovered: update_data.recovered,
+      death: update_data.death
+    )
+  end
+
   def compare_data?(new_data, old_data)
     return false if new_data.infected != old_data.infected
     return false if new_data.active != old_data.active
@@ -72,7 +81,19 @@ class NotificationSender
   end
 
   def send_message(user)
+    MessageSender.new(bot: bot, userid: user.userid, username: user.username, text: message_text).send
+  end
+
+  def message_text
+    case_data = Case.first
+
     text = I18n.t('update_message')
-    MessageSender.new(bot: bot, userid: user.userid, username: user.username, text: text).send
+    text = text.gsub('**last_update**', case_data.updated_at.strftime("%Y-%m-%d %H:%M:%S %z"))
+    text = text.gsub('**infected**', case_data.infected.to_s)
+    text = text.gsub('**active**', case_data.active.to_s)
+    text = text.gsub('**recovered**', case_data.recovered.to_s)
+    text = text.gsub('**death**', case_data.death.to_s)
+    text = text.gsub('**url**', 'indonesia-covid-19.mathdro.id')
+    return text
   end
 end
