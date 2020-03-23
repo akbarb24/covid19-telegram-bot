cc0601438a7764c2d7a44104c2f7389f7ad3f385
Akbar Binaji <akbar.binaji@gmail.com>
2020-03-23 23:10:15 +0800

    update by chnaging load yml file method

--- lib/database_connector.rb
+++ lib/database_connector.rb
@@ -6,7 +6,8 @@ class DatabaseConnector
     def establish_connection
       ActiveRecord::Base.logger = Logger.new(active_record_logger_path)
 
-      configuration = YAML::load(IO.read(database_config_path))
+      # configuration = YAML::load(IO.read(database_config_path))
+      configuration = YAML.load(ERB.new(File.read('database_config_path')).result)
 
       ActiveRecord::Base.establish_connection(configuration)
     end
