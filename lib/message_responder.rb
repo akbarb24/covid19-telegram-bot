93f32b39d9669e4f0ca48f64caef75ec965974c9
Akbar Binaji <akbar.binaji@gmail.com>
2020-03-24 21:47:28 +0800

    add update handler

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -1,4 +1,5 @@
 require './models/user'
+require './models/case'
 require './lib/message_sender'
 
 class MessageResponder
@@ -19,6 +20,10 @@ class MessageResponder
 
     on /^\/help/ do
       handle_help
+    end
+
+    on /^\/update/ do
+      handle_update
     end
 
     on /^\/unsubs/ do
@@ -46,10 +51,15 @@ class MessageResponder
   def handle_start
     subscribe
     answer_with_greeting_message
+    answer_with_update
   end
 
   def handle_help
-    
+    answer_with_help_message
+  end
+
+  def handle_update
+    answer_with_update
   end
 
   def handle_unsubs
@@ -77,7 +87,24 @@ class MessageResponder
     answer_with_message I18n.t('help_message')
   end
 
+  def answer_with_update
+    case_data = get_data
+
+    text = I18n.t('update_message')
+    text = text.gsub('**last_update**', case_data.updated_at.strftime("%Y-%m-%d %H:%M:%S"))
+    text = text.gsub('**infected**', case_data.infected.to_s)
+    text = text.gsub('**active**', case_data.active.to_s)
+    text = text.gsub('**recovered**', case_data.recovered.to_s)
+    text = text.gsub('**death**', case_data.death.to_s)
+    text = text.gsub('**url**', 'indonesia-covid-19.mathdro.id')
+    answer_with_message text
+  end
+
   def answer_with_message(text)
     MessageSender.new(bot: bot, userid: message.chat.id, username: message.chat.username, text: text).send
+  end
+
+  def get_data
+    Case.first
   end
 end
